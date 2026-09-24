5968d42d4bc1597b4041e5071d140005bb05e900
abdalRahmanskaik <124960405+abdalRahmanskaik@users.noreply.github.com>
2026-09-24 22:59:17 +0300

    Add files via upload

---
+++ 02_insertData.sql
@@ -0,0 +1,122 @@
+-- === Insert Data===
+
+-- 1. Insert Doctor Table
+-- -------------------------------
+insert into doctors values
+   ( 1,
+     'Ahmed Hassan',
+     'Cardiology',
+     '0599000001',
+     date '2020-01-15' );
+
+insert into doctors values
+   ( 2,
+     'Sara Ali',
+     'Dermatology',
+     '0599000002',
+     date '2021-03-20' );
+
+insert into doctors values
+   ( 3,
+     'Omar Khaled',
+     'Neurology',
+     '0599000003',
+     date '2019-07-10' );
+
+insert into doctors values
+   ( 4,
+     'Lina Ahmad',
+     'Pediatrics',
+     '0599000004',
+     date '2022-02-05' );
+
+insert into doctors values
+   ( 5,
+     'Mohammed Saleh',
+     'Orthopedics',
+     '0599000005',
+     date '2020-11-18' );
+-- -------------------------------
+-- 2. Insert Patients Table
+-- -------------------------------
+insert into patients values
+   ( 101,
+     'Rania',
+     'Alzaeem',
+     'FEMALE',
+     date '1988-05-10',
+     '0599111111',
+     'Gaza' );
+
+insert into patients values
+   ( 102,
+     'Khaled',
+     'Nasser',
+     'MALE',
+     date '1992-08-15',
+     '0599111112',
+     'Nablus' );
+
+insert into patients values
+   ( 103,
+     'Maya',
+     'Sami',
+     'FEMALE',
+     date '1995-02-20',
+     '0599111113',
+     'Ramallah' );
+
+insert into patients values
+   ( 104,
+     'Yousef',
+     'Omar',
+     'MALE',
+     date '1985-11-03',
+     '0599111114',
+     'Gaza' );
+
+insert into patients values
+   ( 105,
+     'Nour',
+     'Hassan',
+     'FEMALE',
+     date '2000-06-12',
+     '0599111115',
+     'Hebron' );
+-- -------------------------------
+-- 3 Insert Visits Table
+-- -------------------------------
+insert into visits values
+   ( 1001,
+     101,
+     1,
+     date '2026-01-10',
+     'High Blood Pressure',
+     'COMPLETED' );insert into visits values
+   ( 1002,
+     102,
+     2,
+     date '2026-01-15',
+     'Skin Allergy',
+     'COMPLETED' );insert into visits values
+   ( 1003,
+     103,
+     3,
+     date '2026-02-05',
+     'Migraine',
+     'COMPLETED' );insert into visits values
+   ( 1004,
+     104,
+     4,
+     date '2026-02-20',
+     'Flu',
+     'COMPLETED' );insert into visits values
+   ( 1005,
+     105,
+     5,
+     date '2026-03-01',
+     'Back Pain',
+     'SCHEDULED' );
+-- -------------------------------
+
+commit;
